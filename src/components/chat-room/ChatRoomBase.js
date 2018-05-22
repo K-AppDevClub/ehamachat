79b520371ae015e27c23011df8c6549feaefef17
sotaro maehara <s.maehara81@icloud.com>
2018-05-22 20:54:32 +0900

    add optional w&h

--- src/components/chat-room/ChatRoomBase.js
+++ src/components/chat-room/ChatRoomBase.js
@@ -46,15 +46,23 @@ export function generateCanvas() {
           element: stage,
           engine: engine, 
           options: {
-            width: 640, height: 600, 
+            width: this.width, height: this.height, 
             pixelRatio: 2, //Pixel比; スマホ用に2にする
             background: "rgba(0, 20, 20, 120)",
             wireframes: false,
           }
         })
-        var boxB = this.Bodies.rectangle(450, 50, 80, 80);
-        var ground = this.Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-        this.World.add(engine.world, [boxB, ground]);
+        var boxB = this.Bodies.rectangle(450, 50, 80, 80); 
+        var x = [0, this.width, this.width/2]
+        var y = [this.height / 2, 0, this.height]
+        var w = [30, this.width]
+        var h = [this.height, 30]
+        var ground =  this.Bodies.rectangle(this.width/2, this.height,   this.width, 30, { isStatic: true });
+        var groundt = this.Bodies.rectangle(this.width/2, 0,             this.width, 30, { isStatic: true });
+        var groundr = this.Bodies.rectangle(this.width,   this.height/2, 30,         this.height, { isStatic: true });
+        var groundl = this.Bodies.rectangle(0,            this.height/2, 30,         this.height, { isStatic: true });
+        //var ground = this.Bodies.rectangle(this.width/2, this.height-30, this.width, 15, { isStatic: true });
+        this.World.add(engine.world, [boxB, ground, groundl, groundr, groundt]);
         engine.world.gravity.y = 0.1; //重力を0に設定 デフォルトは1
         this.Engine.run(engine);
         this.Render.run(render)
